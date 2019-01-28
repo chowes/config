be300961b7924493f1b09c23da8b688cf7dfe722
chowes <root@colinhowes.com>
2019-01-28 00:18:41 -0500

    Screen lock after resume

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,26 +1,3 @@
 #!/bin/sh
 
-B='#00000000'  # blank
-D='#ff00ffcc'  # default
-T='#ee00ee00'  # text
-W='#dd0048ff'  # wrong
-V='#06eefaff'  # verifying
-
-i3lock                  \
---insidevercolor=$B     \
---ringvercolor=$V       \
-\
---insidewrongcolor=$B   \
---ringwrongcolor=$W     \
-\
---insidecolor=$B        \
---ringcolor=$D          \
---linecolor=$B          \
---separatorcolor=$D     \
-\
---screen 0              \
---blur 5                \
---veriftext=""          \
---wrongtext=""          \
---datestr=""            \
---image="${HOME}/Images/wallpaper" \
+exec i3lock -u -i ${HOME}/Images/lockscreen
